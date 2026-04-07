6b5f6ad07ab0b5e7360374e7594be9631d39494b
nazmul37290 <nazmulnjm@gmail.com>
2026-04-07 20:21:10 +0600

    updated

--- app/(dashboard)/bookings/page.js
+++ app/(dashboard)/bookings/page.js
@@ -12,6 +12,7 @@ export default function Bookings() {
 const router = useRouter();
   useEffect(() => {
     const token = localStorage.getItem("token");
+    console.log(token,'token from bookings page')
     if(!token){
       router.replace("/login");
     }
